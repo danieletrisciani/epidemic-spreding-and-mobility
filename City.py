v. 3.0 -fixed distance formula -added influence of a city estimate -other bug fixes

City.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import math
+import NetworkX as nx
 
 #the function which to solve the differential equation
 def deriv(y, t, N, beta, gamma):
@@ -20,10 +21,15 @@ class City:
         self.occurrences = occurrences
         self.coords =coords
         self.airport_opened = True
+        self.z = 150
 
-        self.S = np.array([self.N - self.occurrences])
-        self.I = np.array([self.occurrences])
+        self.S = np.array([self.N])
+        self.I = np.array([0])
         self.R = np.array([0])
+
+    def getInfected(self):
+
+        return self.I[len(self.I) - 1]
 
     def update(self, cities, beta, gamma, delta_t, t):
 
@@ -38,12 +44,12 @@ class City:
 
         # cities that host an airport has bigger beta than others
         if(self.airport_opened and self.occurrences != 0):
-            beta = (self.occurrences / 200) + beta
+            beta += (self.occurrences/1E4)
 
         for city in cities:
             if(city.name != self.name):
                 dist = self.calcDistance(city)
-                beta += (city.occurrences / 1E4) * math.exp(- dist)
+                beta += (city.getInfected()/1E6) * np.exp(-self.z*dist)
 
         ret = odeint(deriv, y0, t_inter, args=(self.N, beta, gamma))
         S_temp, I_temp, R_temp = ret.T
@@ -51,14 +57,33 @@ class City:
         self.S = np.concatenate([self.S, S_temp[init:]])
         self.I = np.concatenate([self.I, I_temp[init:]])
         self.R = np.concatenate([self.R, R_temp[init:]])
+        lastIndex = len(city.S) - 1
 
     #distance from the city, using the euler distance
     def calcDistance(self, city):
-        dist = np.sqrt((self.coords[0] - city.coords[0])**2 + (self.coords[1] - self.coords[1])**2)
+        def to_rad(grade): return grade * np.pi / 180
+        
+        # print(city.name)
+        # print(self.coords[0])
+        # print(city.coords[0])
+
+        x0 = np.cos(to_rad(self.coords[0]))
+        x1 = np.cos(to_rad(city.coords[0]))
+        y0 = np.sin(to_rad(self.coords[1]))
+        y1 = np.sin(to_rad(city.coords[1]))
+
+        dist = np.sqrt(np.abs((x0 - x1))**2 + np.abs((y0 - y1))**2)
         # print(dist, self.name, city.name)
         return dist
 
-    def plotData(self):
+    def plotData(self, normalized = False):
+
+        ylabel_text = "Population"
+
+        dem = 1
+        if(normalized): 
+            dem = self.N
+            ylabel_text = "Normalized Population"
 
         t = np.linspace(0, len(self.S), len(self.S))
 
@@ -66,12 +91,11 @@ class City:
         fig = plt.figure(facecolor='w')
         ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
         ax.set_title(self.name)
-        ax.plot(t, self.S/self.N, 'b', alpha=0.5, lw=2, label='Susceptible')
-        ax.plot(t, self.I/self.N, 'r', alpha=0.5, lw=2, label='Infected')
-        ax.plot(t, self.R/self.N, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
-        ax.set_xlabel('Time /days')
-        ax.set_ylabel('Normalized Population')
-        ax.set_ylim(0, 1.2)
+        ax.plot(t, self.S/dem, 'b', alpha=0.5, lw=2, label='Susceptible')
+        ax.plot(t, self.I/dem, 'r', alpha=0.5, lw=2, label='Infected')
+        ax.plot(t, self.R/dem, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+        ax.set_xlabel('Days')
+        ax.set_ylabel(ylabel_text)
         ax.yaxis.set_tick_params(length=0)
         ax.xaxis.set_tick_params(length=0)
         ax.grid(b=True, which='major', c='w', lw=2, ls='-')
@@ -79,6 +103,7 @@ class City:
         legend.get_frame().set_alpha(0.5)
         for spine in ('top', 'right', 'bottom', 'left'):
             ax.spines[spine].set_visible(False)
+        plt.savefig('plots/{}_plot.png'.format(self.name))
         plt.show()
 
 
@@ -87,6 +112,6 @@ class City:
 
     def calcInitData(self, cities):
         for city in cities:
-            if(self.name != city.name):
-                dist = self.calcDistance(city)
-                self.I[0] += city.occurrences * math.exp(-dist)
+            dist = self.calcDistance(city)
+            self.I[0] += city.occurrences * np.exp(-self.z * dist)
+        self.S[0] = self.S[0] - self.I[0]

DatabaseCreation/database_creation.py
@@ -3,20 +3,21 @@ import numpy as np
 
 from util import generateDB
 
-fileAlreadyExist = True
+fileAlreadyExist = False
 
 data_folder = "../databases/"
 
 #read and merge of data of population and geographic coordinantes
-popo_df = pd.read_csv(data_folder + "italy_cities.csv", sep='\t', usecols=[0, 1, 7], header=None)
+popo_df = pd.read_csv(data_folder + "italy_cities.csv", sep='\t', usecols=[0, 1, 7], header=None, encoding='latin-1')
 popo_df.columns = ["Code", "Name", "Population"]
 popo_df = popo_df.set_index("Code")
 big_pop_df = popo_df[popo_df['Population'] > 60000]
 
 geo_column = ["Code", "Name", "Longitude", "Latitude"]
-geo_df = pd.read_csv(data_folder + "italy_geo.csv", sep='\t', names=geo_column)
+geo_df = pd.read_csv(data_folder + "italy_geo.csv", sep='\t', names=geo_column, encoding='latin-1')
 geo_df = geo_df.set_index("Code")
 
+#here is created an array that contains all the index to remove, of cities of less than 60000 thousands
 index_to_remove = []
 for index, row in geo_df.iterrows():
     if(not (index in big_pop_df.index)):
@@ -38,8 +39,8 @@ final_df = cities_df.merge(city_occ_df, left_on='Name', right_on='City', how='ou
 final_df = final_df.drop('City', axis=1)
 
 for index, row in final_df.iterrows():
-    if(np.isnan(row['Occurrence'])): 
-        print('ciao')
+    if(np.isnan(row['Occurrence'])):
+        
         final_df.Occurrence[index] = 0
 
 #save the complete database on file

DatabaseCreation/util.py
@@ -6,46 +6,53 @@ data_folder = "../databases/"
 def generateDB(cities_df):
     #read and compute data of airports and routes
 
-    airports_df = pd.read_csv(data_folder + "Airports.csv", usecols=[0, 3, 4, 6, 7], header=None)
+    airports_df = pd.read_csv(data_folder + "airports.csv", usecols=[0, 3, 4, 6, 7], header=None)
     airports_df.columns = ["Code", "State", "AirCode", "Latitude", "Longitude"]
     airports_df = airports_df.set_index('Code')
     it_airports_df = airports_df[airports_df['State'] == 'Italy']
     it_airports_df = it_airports_df[airports_df['AirCode'] != '\\N']
     it_airports_df.reset_index(drop=True, inplace=True)
 
-    routes_df = pd.read_csv(data_folder + "Routes.csv", usecols=[2, 4], header=None)
+    routes_df = pd.read_csv(data_folder + "routes.csv", usecols=[2, 4], header=None)
     routes_df.columns = ["Departure", "Destinations"]
 
-    #rows of the routes database that consider italian airport are deleted
+    #rows of the routes database that not consider italian airport are deleted
     i = 0
     index_to_remove = []
+    number_of_row = len(routes_df.index)
     for index_routes, row_routes in routes_df.iterrows():
         remove = True
-        for index_air, row_air in it_airports_df.iterrows():
-            if(row_air["AirCode"] == row_routes["Departure"]):
+        for index_air, row_it_air in it_airports_df.iterrows():
+            if(row_it_air["AirCode"] == row_routes["Departure"]):
                 remove = False
                 break
 
         if(remove): index_to_remove.append(index_routes)
         i += 1
-        if(i % 200 == 0): print(i)
+        if(i % 200 == 0): print("index of the route database row: {}/{}".format(i, number_of_row))
 
     routes_df = routes_df.drop(index_to_remove)
 
+    #Here airports are linked to the nearest city
     cities = []
     occ = []
+    def to_rad(grade): return grade * np.pi / 180
     for index_air, row_air in it_airports_df.iterrows():
-        distance = 1000
+        distance = 10000
+        x_air = np.cos(to_rad(row_air['Longitude']))
+        y_air = np.sin(to_rad(row_air['Latitude']))
         cities.append('')
         occ.append(0)
         #calculation of the city the own the airport based on the distance of geographic coordinates
         for index_city, row_city in cities_df.iterrows():
-            latitude = row_city['Latitude'] - row_air['Latitude']
-            longitude= row_city['Longitude'] - row_air['Longitude']
-            temp = np.sqrt(latitude**2 + longitude**2)
             
-            if(distance > temp):
-                distance = temp
+            x_city = np.cos(to_rad(row_city['Longitude']))
+            y_city = np.sin(to_rad(row_city['Latitude']))
+
+            temp_distance = np.sqrt((x_air - x_city)**2 + (y_air - y_city)**2)
+            
+            if(distance > temp_distance):
+                distance = temp_distance
                 cities[index_air] = row_city['Name']
 
         #calculations of the number of departures from airports

Italy.py
@@ -3,6 +3,7 @@ from City import City
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import NetworkX as nx
 
 #clear the console for keep the output clear
 def clearConsole():
@@ -19,14 +20,13 @@ class Italy:
 
         #creation of the city objects
         for index, row in city_dataframe.iterrows():
-            coor = [row['Latitude'], row["Longitude"]]
+            coor = [row["Longitude"], row['Latitude']]
 
             self.cities.append(City(row['Name'], row['Population'], row['Occurrence'], coor))
 
         #calcutate the initial data as the initiale infected and susceptibles
         for city in self.cities:
             city.calcInitData(self.cities)
-
         
         self.time = 0
         self.delta_time = 1
@@ -73,6 +73,7 @@ class Italy:
         legend.get_frame().set_alpha(0.5)
         for spine in ('top', 'right', 'bottom', 'left'):
             ax.spines[spine].set_visible(False)
+        plt.savefig('plots/Italy_plot.png')
         plt.show()
 
     
@@ -98,7 +99,7 @@ class Italy:
             return False
 
     #show the plot of the SIR model of a one or more city
-    def showPlots(self, *names):
+    def showPlots(self, *names, normalized = False):
         
         found = False
 
@@ -106,7 +107,7 @@ class Italy:
             for city in self.cities:
 
                 if (city.name == name):
-                    city.plotData()
+                    city.plotData(normalized = normalized)
                     found = True
                     break
         
@@ -115,34 +116,36 @@ class Italy:
     #find the city with the most infected people, and the city with the least infected people
     #this function is only useful launched at the end of the simulation
     #the function return a tuple with the two names of the cities
-    def findMostLowInfCity(self):
+    def findMaxMinInfCity(self, normalized = False):
 
-        #find most infected city
-        name_most_inf = ''
-        most_infect = 0
+        #find max infected city
+        name_max_inf = ''
+        max_infect_norm = 0
+
+        #find min infected city
+        name_min_inf = ''
+        min_infect_norm = 60E6
 
         for city in self.cities:
-            greater = np.max(city.I)/city.N
+
+            den = 1
+            if(normalized): den = city.N
+            infected = np.max(city.I)
+            infected_norm = infected/den
             
-            if(greater > most_infect):
-                most_infect = greater
-                name_most_inf = city.name
-        
-        #find least infected city
-        name_least_inf = ''
-        least_infect = 1
+            if(infected_norm > max_infect_norm):
+                max_infect_norm = infected_norm
+                name_max_inf = city.name
 
-        for city in self.cities:
-            minor = np.max(city.I)/city.N
-            if(minor < least_infect):
-                least_infect = minor
-                name_least_inf = city.name
+            if(infected_norm < min_infect_norm):
+                min_infect_norm = infected_norm
+                name_min_inf = city.name
 
-        return name_most_inf, name_least_inf
+        return (name_max_inf, max_infect_norm), (name_min_inf, min_infect_norm)
 
     def printAllData(self, normalized = False):
 
-        clearConsole()
+        #clearConsole()
 
         names = []
         pop = []
@@ -151,7 +154,7 @@ class Italy:
         rem = []
         occ = []
 
-        print("Day: ", self.time - 1)
+        print("Day: ", self.time)
 
         for city in self.cities:
 
@@ -163,6 +166,10 @@ class Italy:
             names.append(city.name)
             pop.append(city.N)
             if div == 1: 
+                if(city == "Roma"): 
+                    print(city.S[lastIndex])
+                    print(city.I[lastIndex])
+                    print(city.R[lastIndex])
                 sus.append(round(city.S[lastIndex] / div))
                 inf.append(round(city.I[lastIndex] / div)) 
                 rem.append(round(city.R[lastIndex] / div)) 
@@ -173,8 +180,8 @@ class Italy:
 
             occ.append(city.occurrences)
         
-        temp_df = pd.DataFrame({"City": names, "Population": pop, "Susceptible": sus, "Infectious": inf, "Removed": rem, "Airport connection": occ})
-        temp_df = temp_df.astype({'Airport connection': 'int32'})
+        temp_df = pd.DataFrame({"City": names, "Population": pop, "Susceptible": sus, "Infectious": inf, "Removed": rem, "Airport routes": occ})
+        temp_df = temp_df.astype({'Airport routes': 'int32'})
         temp_df = temp_df.sort_values(by=['Population'], ascending=False, ignore_index = True)
 
         with pd.option_context('expand_frame_repr', False, 'display.max_columns', None, 'display.max_rows', None):  # more options can be specified also
@@ -186,26 +193,86 @@ class Italy:
             status = "Closed"
         print("Airport: " + status)
 
-data_folder = "../databases/"
+    def createInfluenceTable(self):
+        
+        z = 150
+        names = []
+        lst_sum_weights = []
+
+        for city_one in self.cities:
+            names.append(city_one.name)
+            sum_weights = 0
+            for city_two in self.cities:
+
+                if(city_one.name != city_two.name):
+
+                    dist = city_one.calcDistance(city_two)
+                    sum_weights += np.exp(-z*dist)
+
+            lst_sum_weights.append(sum_weights)
+
+        for i in range(len(names)):
+            print(names[i], lst_sum_weights[i])
+
+        temp_df = pd.DataFrame({"City": names, "Influenced rate": lst_sum_weights})
+        temp_df = temp_df.sort_values(by=['Influenced rate'], ascending=False, ignore_index = True)
+        with pd.option_context('expand_frame_repr', False, 'display.max_columns', None, 'display.max_rows', None):  # more options can be specified also
+            print(temp_df)
+
+def printWeight(italy):
+    city_selected = None
+    for city in italy.cities:
+
+        if city.name == "Roma":
+            city_selected = city
+            break
+
+    i = 0
+    for city in italy.cities:
+
+        if city.name == "Roma":
+            continue
+        
+        dist = city_selected.calcDistance(city)
+        weight = np.exp(-150*dist)
+
+        print("{}&{}-{}&{} {}\n\\hline".format(i, city_selected.name, city.name, weight, dist))
+        i += 1
+
+def startSim(italy):
+
+    # print data at time t = 0
+    italy.printAllData()
+
+    while(italy.updateModel()):
+        
+        days_to_print = [10, 45, 60, 125, italy.stop_sim]
+
+        if(italy.time in days_to_print):
+            italy.printAllData()
+
+            if(italy.time == italy.stop_sim):
+                italy.calc_total_infection()
+
+
+    (max_city, max_infected_people), (min_city, min_infected_people) = italy.findMaxMinInfCity()
+    (max_city_norm, max_infected_people_norm), (min_city_norm, min_infected_people_norm) = italy.findMaxMinInfCity(normalized = True)
+
+    print("Minimum and maximum not normalized: {} ({}), {} ({})".\
+        format(min_city, min_infected_people, max_city, max_infected_people))
+    print("Minimum and maximum normalized: {} ({}), {} ({})".\
+        format(min_city_norm, min_infected_people_norm, max_city_norm, max_infected_people_norm))
+    italy.showPlots(min_city)
+    italy.showPlots(max_city)
+    italy.showPlots(min_city_norm, normalized = True)
+    italy.showPlots(max_city_norm, normalized = True)
+
+data_folder = "databases/"
 allinfo_df = pd.read_csv(data_folder + "allinfo.csv")
 
 italy = Italy(allinfo_df, )
+italy.createInfluenceTable()
 
-#print data at time t = 0
-# italy.printAllData()
+# startSim(italy)
 
-while(italy.updateModel()):
-    # italy.printAllData()
-    if(italy.time == italy.stop_sim):
-        # italy.showPlots("Fiumicino")
-        italy.printAllData()
-        italy.calc_total_infection()
-        pass
-    pass
 
-low_more_city = italy.findMostLowInfCity()
-
-print(low_more_city)
-
-italy.showPlots(low_more_city[0])
-italy.showPlots(low_more_city[1])